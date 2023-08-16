dcb046af8a06510b270728f7bfe92aa4c11a4686
L. Tanawut <54799037+ongloveth@users.noreply.github.com>
2023-08-16 18:46:49 +0700

    update zones

---
+++ zones.config.lua
@@ -0,0 +1,39 @@
+Config = {}
+
+Config.zones = {
+    {
+        radius = 30.0,
+        limit = 10.0,
+        coords = vector3(-1963.352, 2982.3747, 32.801803),
+        markerColor = {
+            r = 255,
+            g = 0,
+            b = 0,
+            a = 0.3
+        },
+    },
+
+    {
+        radius = 10.0,
+        limit = 15.0,
+        coords = vector3(-2081.688, 3056.4414, 32.810371),
+        markerColor = {
+            r = 255,
+            g = 255,
+            b = 255,
+            a = 0.3
+        },
+    },
+
+    {
+        radius = 10.0,
+        limit = 20.0,
+        coords = vector3(-2209.936, 3127.8476, 32.788673),
+        markerColor = {
+            r = 0,
+            g = 0,
+            b = 0,
+            a = 0.3
+        },
+    },
+}
